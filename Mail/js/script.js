60324971de3e12609c921b5bb6ba29aa42455d88
bigazzi96 <fbigazzi96@gmail.com>
2024-05-13 22:46:57 +0200

    aggiunte condizioni (esercizio mail)

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,5 +1,17 @@
 
-const username = [frank, john, kevin, jimmy, tim, tom];
+const listmail = ['frank@gmail.com, john@gmail.com, kevin@gmail.com, jimmy@gmail.com, tim@gmail.com, tom@gmail.com'];
 
-const  email = prompt(qual'è la tua e-mail?);
+let email prompt('scrivi la tua email');
 
+console.log(email);
+
+if (listmail.includes(email))
+{
+    console.log (puoi accedere);
+}
+
+else {
+    console.log(non puoi accedere);
+}
+
+const email = document.getElementById("email");
