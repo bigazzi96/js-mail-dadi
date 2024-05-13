8e054a6617bab534caae6d8d93d74a2cf63df4ef
bigazzi96 <fbigazzi96@gmail.com>
2024-05-13 18:29:53 +0200

    aggiunte condizioni per macchina e umano

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,7 +1,4 @@
 
-
-const winner = document.getElementById('vincitore');
-
 const max = 6
 
 //* NUMERO  CASUALE PER UMANO
@@ -9,4 +6,19 @@ const giocatore = Math.floor(Math.random()* max + 1);
 
 //* NUMERO CASUALE PER MACCHINA
 const macchina = Math.floor(Math.random()* max + 1);
+
+
+if (giocatore > macchina){
+    vincitore.innerText= ("Il Giocatore ha vinto")
+}
+
+else if (giocatore < macchina){
+    vincitore.innerText= ("La macchina ha  vinto")
+}
+
+const vincitore = document.getElementById("vincitore");
+
+console.log(giocatore,macchina);
+
+
 
