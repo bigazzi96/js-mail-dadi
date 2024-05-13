29e5a2fabbbba75c882ff583e4dc45c2637be93e
bigazzi96 <fbigazzi96@gmail.com>
2024-05-13 17:40:15 +0200

    added const per  giocatore  e macchina

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,12 @@
+
+
+const winner = document.getElementById('vincitore');
+
+const max = 6
+
+//* NUMERO  CASUALE PER UMANO
+const giocatore = Math.floor(Math.random()* max + 1);
+
+//* NUMERO CASUALE PER MACCHINA
+const macchina = Math.floor(Math.random()* max + 1);
+
